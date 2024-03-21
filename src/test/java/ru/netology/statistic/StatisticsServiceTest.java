78ee338f3b6a96b9bbdab546b50aa27338d6d6ac
num13 <num13-ne@mail.ru>
2024-03-21 17:32:54 +0300

    Formated Code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void  findIfCurrentLessThenIncome(){
+    void findIfCurrentLessThenIncome() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
